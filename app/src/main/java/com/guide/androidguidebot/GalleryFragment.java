717d0129576ad9266034efd6ea60b6c56ea6aa2d
Sobol-R <rostislav.sobolev@gmail.com>
2018-04-16 16:32:51 +0300

    first commit

---
+++ app/src/main/java/com/guide/androidguidebot/GalleryFragment.java
@@ -0,0 +1,25 @@
+package com.guide.androidguidebot;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageSwitcher;
+import android.widget.ImageView;
+
+public class GalleryFragment extends Fragment {
+    public GalleryFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        //return inflater.inflate(R.layout.gallery_without_pictures, container, false);
+        return inflater.inflate(R.layout.gallery, container, false);
+
+    }
+
+}
